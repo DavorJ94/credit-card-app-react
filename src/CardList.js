ae676a2006e0f7953e45c7c44822885dab9130ba
DavorJ94 <davorjovanovic94@gmail.com>
2021-02-23 23:01:34 +0100

    added additional functionality

--- src/CardList.js
+++ src/CardList.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CreateCreditCard from "./CreateCreditCard";
 import "./styles/CardList.css";
 function CardList() {
-  const items = Object.values({ ...localStorage });
+  const [storage, setStorage] = useState(Object.values({ ...localStorage }));
 
+  const removeCard = (e) => {
+    localStorage.removeItem(e.target.id);
+    setTimeout(() => {
+      setStorage(Object.values({ ...localStorage }));
+    }, 200);
+  };
+
+  const items = storage;
   const allCards = items.map((item) => {
     let element = JSON.parse(item);
     return (
-      <Link to={`/cards/${element.id}/edit`} className="links" key={element.id}>
-        <CreateCreditCard
-          elements={element}
+      <div className="card-and-btn-container" key={element.id}>
+        <Link
+          to={`/cards/${element.id}/edit`}
+          className="links"
           key={element.id}
-          showHide={false}
-        />
-      </Link>
+        >
+          <CreateCreditCard
+            elements={element}
+            key={element.id}
+            showHide={false}
+          />
+        </Link>
+        <button className="btn" onClick={removeCard}>
+          <i id={element.id} className="fas fa-trash-alt"></i>
+        </button>
+      </div>
     );
   });
   return (
